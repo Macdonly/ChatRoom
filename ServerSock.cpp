5c901396455adf88e50bf52d75a7c9dc02cb0a93
dai <995417970@qq.com>
2021-02-18 23:19:02 +0800

    多进程实现服务端
    合理处理僵尸进程

--- ServerSock.cpp
+++ ServerSock.cpp
@@ -1,3 +1,4 @@
+#include <sys/signal.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -13,12 +14,12 @@ namespace ChatRoom
     ServerSock::ServerSock(int port, const char* ip) : m_port(port), m_IP(ip)
     {
 	fd = socket(PF_INET,SOCK_STREAM,0); //PF_INET表示TCP连接，SOCK_STREAM表示基于流，第三个参数一般为0
-    bzero(&m_addr,sizeof(m_addr));
+	bzero(&m_addr,sizeof(m_addr));
 	m_addr.sin_family = AF_INET;
 	m_addr.sin_port = htons(port);
-    //m_addr.sin_addr.s_addr = ::inet_addr(ip);
-    //inet_pton(AF_INET,ip,&m_addr.sin_addr);
-    m_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//m_addr.sin_addr.s_addr = ::inet_addr(ip);
+	//inet_pton(AF_INET,ip,&m_addr.sin_addr);
+	m_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     }
 
     void ServerSock::bind()
@@ -38,6 +39,7 @@ namespace ChatRoom
     void ServerSock::listen()
     {
 	int err = ::listen(fd,5);//参数5表示内核监听队列的最大长度为5，超过5则不受理新的客户连接
+	signal(SIGCHLD,SIG_IGN);	
 	if(err >= 0)
 	{
 	    cout << "listen succeed." << err<<endl;
@@ -50,22 +52,31 @@ namespace ChatRoom
     }
     void ServerSock::accept()
     {
+	//signal(SIGCHLD,SIG_IGN);	
+	cout << "pid:" << getpid() << endl;	
 	sockaddr_in client;
 	socklen_t client_addrlen = sizeof(client);
 	int err = ::accept(fd,(struct sockaddr*)&client,&client_addrlen);
-    //cout << err << endl;
+	//cout << err << endl;
 	if(err >= 0)
 	{
 	    cout << "connect succeeded "<< endl;
 	    cout << "client port: "<<ntohs(client.sin_port) << endl;
-	    recv(err);
+
+	    int ipid = fork();
+	    if(!ipid)
+	    {
+		recv(err);
+		exit(0);
+	    }
+
 	}
 	else
 	{
 	    exit(0);
 	    cout <<"connect failed" << endl;
 	}
-	
+
     }
 
     void ServerSock::recv(int rwsocket)
@@ -78,18 +89,22 @@ namespace ChatRoom
 	    int err = ::recv(rwsocket,recvBuf,19,0);
 	    if(err > 0)
 	    {
-		cout <<"接收的字节数："<< err << endl;
-	    	for(int i=0; i<sizeof(recvBuf); i++)
+	    	cout <<"接收的字节数："<< err << endl;
+		for(int i=0; i<sizeof(recvBuf); i++)
 		{
 		    cout << recvBuf[i];
 		}
 		cout << endl;
+		memset(recvBuf,'\0',20);
 	    }
 	    else
 	    {
+		if(close(err) == 0)
+		{
+		    cout << "client has closed the connection" << endl;	
+		}
 		break;
 	    }
-	    memset(recvBuf,'\0',20);
 	}
     }
 
